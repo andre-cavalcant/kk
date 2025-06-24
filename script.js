39a4347849109f9dd0aa4ba34b3307066fc18e74
andre-cavalcant <andre.cavalcante.silva@escola.pr.gov.br>
2025-06-24 09:31:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ mensagem.textContent = "Você clicou no botão! 🎉";
 
 
 }
+document.addEventListener("DOMContentLoaded",
+function () {
+
+
+   
+window.mostrarMensagem = function () {
+
+
+        const mensagem
+= document.getElementById("mensagem");
+
+
+       
+mensagem.textContent = "Você clicou no botão! 🎉";
+
+
+    };
+
+
+});
